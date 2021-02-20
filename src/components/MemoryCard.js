55c6d2c64fd24332c19e7e5a94e042ad5e580b06
Tilda Udufo <mathildaudufo@gmail.com>
2021-02-20 23:05:03 +0100

    Display images

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
 import khaleesi from "../images/khaleesi.jpg";
 import arya from "../images/arya.jpg";
 import cersei from "../images/cersei.jpg";
@@ -9,16 +9,24 @@ import margaret from "../images/margaret.jpg";
 import sansa from "../images/sansa.jpg";
 import tyrion from "../images/tyrion.jpg";
 
-function MemoryCard() {
+export default function MemoryCard() {
 	const initImgArr = [
-		{ src: khaleesi, id: uniqid(), clicked: false },
-		{ src: jon, id: uniqid(), clicked: false },
-		{ src: arya, id: uniqid(), clicked: false },
-    { src: cersei, id: uniqid(), clicked: false },
-    { src: margaret, id: uniqid(), clicked: false },
-    { src: sansa, id: uniqid(), clicked: false },
-    { src: tyrion, id: uniqid(), clicked: false },
-    { src: jaime, id: uniqid(), clicked: false },
+		{ src: khaleesi, id: uniqid(), clicked: false, name: "Daenerys Targaryen" },
+		{ src: jon, id: uniqid(), clicked: false, name: "Jon Snow" },
+		{ src: arya, id: uniqid(), clicked: false, name: "Arya Stark" },
+		{ src: cersei, id: uniqid(), clicked: false, name: "Cersei Lannister" },
+		{ src: margaret, id: uniqid(), clicked: false, name: "Margaery Tyrell" },
+		{ src: sansa, id: uniqid(), clicked: false, name: "Sansa Stark" },
+		{ src: tyrion, id: uniqid(), clicked: false, name: "Tyrion Lannister" },
+		{ src: jaime, id: uniqid(), clicked: false, name: "Jaime Lannister" },
 	];
 	const [imgArr, setImgArr] = useState(initImgArr);
+
+	return (
+		<div>
+			{imgArr.map((img) => (
+				<img src={img.src} alt={img.name} />
+			))}
+		</div>
+	);
 }
